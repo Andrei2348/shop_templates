a11370153755c01e1037fa562c705789bd16408c
Andrei2348 <makarevichandco@gmail.com>
2024-01-12 18:59:33 +0300

    Добавил немного стилей

--- forms.py
+++ forms.py
@@ -4,8 +4,8 @@ from wtforms.validators import DataRequired, Email, Length, EqualTo
 
 
 class RegistrationForm(FlaskForm):
-    first_name = StringField('First_name', validators=[DataRequired()])
-    last_name = StringField('Last_name', validators=[DataRequired()])
+    first_name = StringField('Имя пользователя', validators=[DataRequired()])
+    last_name = StringField('Фамилия пользователя', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired(), Length(min=8)])
-    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    password = PasswordField('Введите пароль', validators=[DataRequired(), Length(min=8)])
+    confirm_password = PasswordField('Подтвердите пароль', validators=[DataRequired(), EqualTo('password')])
