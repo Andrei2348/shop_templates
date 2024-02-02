fb95e36c3395c454213d352698d30613263fca9c
Andrei2348 <makarevichandco@gmail.com>
2024-02-02 22:06:00 +0300

    Доработка домашки

--- homework/myapp/views.py
+++ homework/myapp/views.py
@@ -3,18 +3,23 @@ from .models import Orders
 from datetime import date, timedelta
 
 
-
-def cart(request, index):
-    order = Orders.objects.filter(customer = index)
+def create_result(order):
     order_list = []
     username = order[0].customer
     for i in range(len(order)):
-        order_list.append(list(order[i].product.all()))
+        order_list.append(tuple(order[i].product.all()))
+    return order_list, username
+
+
+def cart(request, index):
+    order = Orders.objects.filter(customer = index)
+    order_list, username = create_result(order)
     context = {
     'title': 'Все заказы',
     'username': username,
     'orders': order_list
     }
+    
     return render(request, 'myapp/index.html', context=context)
 
 
@@ -22,10 +27,7 @@ def cart_week(request, index):
     today = date.today()
     delta = today - timedelta(days=7)
     order = Orders.objects.filter(customer = index, date_reg__gte=delta)
-    order_list = []
-    username = order[0].customer
-    for i in range(len(order)):
-        order_list.append(list(order[i].product.all()))
+    order_list, username = create_result(order)
     context = {
     'title': 'Все заказы за неделю',
     'username': username,
@@ -38,10 +40,7 @@ def cart_mounth(request, index):
     today = date.today()
     delta = today - timedelta(days=30)
     order = Orders.objects.filter(customer = index, date_reg__gte=delta)
-    order_list = []
-    username = order[0].customer
-    for i in range(len(order)):
-        order_list.append(list(order[i].product.all()))
+    order_list, username = create_result(order)
     context = {
     'title': 'Все заказы за месяц',
     'username': username,
@@ -54,13 +53,11 @@ def cart_year(request, index):
     today = date.today()
     delta = today - timedelta(days=365)
     order = Orders.objects.filter(customer = index, date_reg__gte=delta)
-    order_list = []
-    username = order[0].customer
-    for i in range(len(order)):
-        order_list.append(list(order[i].product.all()))
+    order_list, username = create_result(order)
     context = {
     'title': 'Все заказы за год',
     'username': username,
     'orders': order_list
     }
     return render(request, 'myapp/index.html', context=context)
+
